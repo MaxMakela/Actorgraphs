6002492a233889ba31f2e4c57313c0822c07d33e
max <max@gmail.com>
2018-12-23 19:52:28 +0200

    generated bad graph

---
+++ graph_creating.py
@@ -0,0 +1,38 @@
+from graph import ActorsGraph
+import csv
+import imdb
+from imdb.Person import Person
+
+id_list = []
+movie_id_list = []
+ia = imdb.IMDb()
+
+def get_filmography(imdb_person):
+    filmography = []
+    for step in imdb_person:
+        if 'actor' in step:
+            filmography = step['actor']
+            break
+    return filmography
+    
+with open('actors_id.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        id_list.append(row[0])
+
+graph = ActorsGraph(id_list)
+
+
+for actor in id_list[:10]:
+    for film in get_filmography(ia.get_person(actor)['filmography']):
+        for cast_actor in ia.get_movie(film.movieID)['cast']:
+            if cast_actor.personID in id_list[:10]:
+                graph.add_edge(actor, cast_actor.personID, film.movieID)
+
+            
+
+#print(len(movie_id_list))
+    
+graph.save()
+
+#print(ia.get_person(id_list[6])['filmography'][0])
