11589543b2fa5d6646fdeb55143449afb3f1e5fc
max <max@gmail.com>
2018-12-27 14:14:06 +0200

    add new actor's edges

--- graph_generator.py
+++ graph_generator.py
@@ -1,6 +1,7 @@
 import imdb
 from log import log
 from graph import ActorsGraph
+import itertools
 
 
 class GraphGenerator(object):
@@ -18,6 +19,10 @@ class GraphGenerator(object):
                 break
         return filmography
 
+    def combination(cast):
+        cast_id = [actor.personID for actor in cast]
+        return list(itertools.combinations(cast_id, 2))
+
     def generate(self):
         """Generate graph for actors using IMDB API"""
         graph = ActorsGraph(self.actor_ids)
@@ -26,10 +31,13 @@ class GraphGenerator(object):
         for actor_id in graph.actor_ids:
             person = self.ia.get_person_main(actor_id)
             log.info("imdb person found: {}: {}".format(actor_id, person["data"]["name"]))
-
             for film in self.get_filmography(person["data"]['filmography']):
                 if film.movieID not in films_ids_set:
                     films_ids_set.add(film.movieID)
+                    
+                    for actor_duo in combination(self.ia.get_movie(film.movieID)['cast']):
+                        graph.add_edge(actor_duo[0], actor_duo[1], film.movieID)
+
                     for cast_actor in self.ia.get_movie(film.movieID)['cast']:
                         if cast_actor.personID in self.actor_ids_set:
                             log.info("adding edge {}, {}: {} '{}'".format(
