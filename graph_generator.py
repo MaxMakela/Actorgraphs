da2b0f2708e4d10357147e6a77cce362a357858f
max <max@gmail.com>
2019-01-09 23:37:43 +0200

    broken new version graph generating

--- graph_generator.py
+++ graph_generator.py
@@ -23,26 +23,46 @@ class GraphGenerator(object):
     #     cast_id = [actor.personID for actor in cast]
     #     return list(itertools.combinations(cast_id, 2))
 
+
     def generate(self):
         """Generate graph for actors using IMDB API"""
-        graph = ActorsGraph(self.actor_ids)
-        films_ids_set = set()
+        graph = ActorsGraph(self.actor_ids)  
+        films_ids_dict = {}
 
         for actor_id in graph.actor_ids:
             person = self.ia.get_person_main(actor_id)
-            log.info("imdb person found: {}: {}".format(actor_id, person["data"]["name"]))
             for film in self.get_filmography(person["data"]['filmography']):
-                if film.movieID not in films_ids_set:
-                    films_ids_set.add(film.movieID)
+                if film.movieID in films_ids_dict:
+                    films_ids_dict[film.movieID]+=1
+                else:
+                    films_ids_dict[film.movieID]=1
 
-                    for actor_duo in list(itertools.combinations([actor.personID for actor in self.ia.get_movie(film.movieID)['cast']], 2)):
-                         if actor_duo[0] in self.actor_ids_set and actor_duo[1] in self.actor_ids_set:
-                            log.info("adding edge {}, {}: {} '{}'".format(
-                                actor_duo[0],
-                                actor_duo[1],
-                                film.movieID,
-                                film.data["title"]
-                            ))
-                            graph.add_edge(actor_duo[0], actor_duo[1], film.movieID)
+        films_ids_list = [i for i in films_ids_dict if films_ids_dict[i]!=1]
+
+        for film in films_ids_list:
+            actors_in_cast = []
+            for actor in self.ia.get_movie(film)['cast']:
+                if actor.personID in actor_id:
+                    actors_in_cast.append(actor.personID)
+            for actor_duo in list(itertools.combinations([actor for actor in actors_in_cast], 2)):
+                graph.add_edge(actor_duo[0], actor_duo[1], film)        
+
+
+        # for actor_id in graph.actor_ids:
+        #     person = self.ia.get_person_main(actor_id)
+        #     log.info("imdb person found: {}: {}".format(actor_id, person["data"]["name"]))
+        #     for film in self.get_filmography(person["data"]['filmography']):
+        #         if film.movieID not in films_ids_set:
+        #             films_ids_set.add(film.movieID)
+
+        #             for actor_duo in list(itertools.combinations([actor.personID for actor in self.ia.get_movie(film.movieID)['cast']], 2)):
+        #                  if actor_duo[0] in self.actor_ids_set and actor_duo[1] in self.actor_ids_set:
+        #                     log.info("adding edge {}, {}: {} '{}'".format(
+        #                         actor_duo[0],
+        #                         actor_duo[1],
+        #                         film.movieID,
+        #                         film.data["title"]
+        #                     ))
+        #                     graph.add_edge(actor_duo[0], actor_duo[1], film.movieID)
 
         return graph
